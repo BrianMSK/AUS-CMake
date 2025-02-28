38004bb141d902abc8e460f2cc83fe672de111f3
Denis Uhrík <uhrikdenis@gmail.com>
2025-02-28 13:34:35 +0100

    wip pseudocode implementation of cmm

--- libds/mm/memory_manager.h
+++ libds/mm/memory_manager.h
@@ -1,63 +1,54 @@
 #pragma once
 
 #include <libds/heap_monitor.h>
+#include <uchar.h>
 
 namespace ds::mm {
 
-	template<typename BlockType>
-	class MemoryManager {
+template <typename BlockType> class MemoryManager {
 
-	public:
-		MemoryManager();
-		virtual ~MemoryManager();
+public:
+  MemoryManager();
+  virtual ~MemoryManager();
 
-		virtual BlockType* allocateMemory();
-		virtual void releaseMemory(BlockType* pointer);
+  virtual BlockType *allocateMemory();
+  virtual void releaseMemory(BlockType *pointer);
 
-		void releaseAndSetNull(BlockType*& pointer);
+  void releaseAndSetNull(BlockType *&pointer);
 
-		size_t getAllocatedBlockCount() const;
+  size_t getAllocatedBlockCount() const;
 
-	protected:
-		size_t allocatedBlockCount_;
-	};
+protected:
+  size_t allocatedBlockCount_;
+};
 
-	template<typename BlockType>
-    MemoryManager<BlockType>::MemoryManager():
-		allocatedBlockCount_(0)
-	{
-	}
+template <typename BlockType>
+MemoryManager<BlockType>::MemoryManager() : allocatedBlockCount_(0) {}
 
-	template<typename BlockType>
-    MemoryManager<BlockType>::~MemoryManager()
-	{
-		allocatedBlockCount_ = 0;
-	}
-
-	template<typename BlockType>
-    BlockType* MemoryManager<BlockType>::allocateMemory()
-	{
-		allocatedBlockCount_++;
-		return new BlockType();
-	}
-
-	template<typename BlockType>
-    void MemoryManager<BlockType>::releaseMemory(BlockType* pointer)
-	{
-		allocatedBlockCount_--;
-		delete pointer;
-	}
-
-	template<typename BlockType>
-    void MemoryManager<BlockType>::releaseAndSetNull(BlockType*& pointer)
-	{
-		this->releaseMemory(pointer);
-		pointer = nullptr;
-	}
-
-	template<typename BlockType>
-    size_t MemoryManager<BlockType>::getAllocatedBlockCount() const
-	{
-		return allocatedBlockCount_;
-	}
+template <typename BlockType> MemoryManager<BlockType>::~MemoryManager() {
+  allocatedBlockCount_ = 0;
 }
+
+template <typename BlockType>
+BlockType *MemoryManager<BlockType>::allocateMemory() {
+  allocatedBlockCount_++;
+  return new BlockType();
+}
+
+template <typename BlockType>
+void MemoryManager<BlockType>::releaseMemory(BlockType *pointer) {
+  allocatedBlockCount_--;
+  delete pointer;
+}
+
+template <typename BlockType>
+void MemoryManager<BlockType>::releaseAndSetNull(BlockType *&pointer) {
+  this->releaseMemory(pointer);
+  pointer = nullptr;
+}
+
+template <typename BlockType>
+size_t MemoryManager<BlockType>::getAllocatedBlockCount() const {
+  return allocatedBlockCount_;
+}
+} // namespace ds::mm
